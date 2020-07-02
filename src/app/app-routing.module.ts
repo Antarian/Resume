97a2bdfac34e3db681e5ac13dd2703d1c28a5f65
Peter Labos <peter.labos@gmail.com>
2020-07-02 23:20:43 +0100

    R-10 web redesign (part 2)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,38 +3,14 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
     {
-        path: '',
-        redirectTo: 'home',
+        path: 'home',
+        redirectTo: '',
         pathMatch: 'full'
     },
     {
-        path: 'home',
+        path: '',
         loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
     },
-    {
-        path: 'principles',
-        loadChildren: () => import('./dictionary/dictionary.module').then(m => m.DictionaryPageModule)
-    },
-    // {
-    //     path: 'minitaurus',
-    //     loadChildren: () => import('./minitaurus/minitaurus.module').then(m => m.MinitaurusPageModule)
-    // },
-    // {
-    //     path: 'minitaurus-part2',
-    //     redirectTo: 'minitaurus/part2',
-    //     pathMatch: 'full'
-    // },
-    // {
-    //     path: 'minitaurus-part3',
-    //     redirectTo: 'minitaurus/part3',
-    //     pathMatch: 'full'
-    // },
-    // {
-    //     path: 'minitaurus-part4',
-    //     redirectTo: 'minitaurus/part4',
-    //     pathMatch: 'full'
-    // },
-    // { path: 'ionic-aws-deployment', loadChildren: './ionic-aws-deployment/ionic-aws-deployment.module#IonicAwsDeploymentPageModule' }
 ];
 
 @NgModule({
